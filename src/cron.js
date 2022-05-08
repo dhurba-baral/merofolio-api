d0452fa9d7961d0af584c9da8889a300b9929c4f
Dhurba Baral <dhurbabaralam@gmail.com>
2022-05-08 16:28:36 +0545

    initial commit

---
+++ src/cron.js
@@ -0,0 +1,8 @@
+const cron = require('node-cron');
+const shell = require('shelljs');
+
+cron.schedule("* * * * *",function(){
+    if(shell.exec("node src/scraping.js").code!==0){
+        console.log("Something is wrong")
+    }
+})
---
+++ src/scraping.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const express =require("express")
+const app =express()
+process.setMaxListeners(Infinity)
+
+const port = process.env.PORT || 3000;
+
+async function start(){
+    try{
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    
+    await page.goto("https://merolagani.com/LatestMarket.aspx")
+    let data = await page.$$eval('#ctl00_ContentPlaceHolder1_LiveTrading > table> tbody > tr',(rows)=>{
+        let list = [];
+        rows.forEach(row=>{
+            let record = {"Symbol":"","Companyname":"","Ltp":"","Close":""}
+            record.Symbol= row.querySelector("td>a").textContent;
+            fullname = row.querySelector("td>a").title;
+            record.Companyname = fullname.substring( fullname.indexOf( '(' ) + 1, fullname.indexOf( ')' ) )
+            record.Ltp = row.querySelector("td:nth-child(2)").textContent
+            record.Close = row.querySelector("td:nth-child(8)").textContent
+            list.push(record)
+        })
+        return list;
+    }
+    )
+    return data;
+    // await browser.close();
+    }
+    catch(err){
+        console.log(error);
+    }
+}
+
+app.get("/api/livedata",async (req,res)=>{
+    const data = await start()
+    res.status(201).send(data);
+
+})
+
+
+app.listen(port, () => {
+    console.log('Example app listening on port '+port);
+});
