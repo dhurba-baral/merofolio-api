b457c4f2b8265ace91b635b5b78798b840b3c6b2
Dhurba Baral <dhurbabaralam@gmail.com>
2022-05-08 16:55:35 +0545

    name changed

--- src/cron.js
+++ src/cron.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const shell = require('shelljs');
 
 cron.schedule("* * * * *",function(){
-    if(shell.exec("node src/scraping.js").code!==0){
+    if(shell.exec("node src/index.js").code!==0){
         console.log("Something is wrong")
     }
 })
